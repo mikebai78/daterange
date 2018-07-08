0f975a84fffa2a92a6ee200283e693fb5adbb93c
Mingming Bai <mb@Mingmings-Mac-mini.local>
2018-07-08 07:51:51 -0600

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { NgCalendarModule } from 'ionic2-calendar';
+
+
 
 @NgModule({
   declarations: [
@@ -14,7 +17,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    NgCalendarModule
+
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,101 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import moment from 'moment';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public date = moment()
+  daysArr;
+
+  startDate;
+  endDate;
 
   constructor(public navCtrl: NavController) {
+    this.daysArr = this.createCalendar(this.date)
 
   }
+
+  createCalendar(month) {
+    let firstDay = moment(month).startOf('M');
+    let days = Array.apply(null, { length: month.daysInMonth() })
+      .map(Number.call, Number)
+      .map((n) => {
+        return moment(firstDay).add(n, 'd')
+      })
+
+    for (let n = 0; n < firstDay.weekday(); n++) {
+      days.unshift(null);
+    }
+    return days
+  }  
+
+  isReserved(day){
+    if (!day) {
+      return false;
+    }
+    return false
+  }
+  
+  beforeToday(day){
+    if (!day) {
+      return false;
+    }
+    return moment().format('L') > day.format('L');
+  }
+
+  todayCheck(day) {
+    if (!day) {
+      return false;
+    }
+    return moment().format('L') === day.format('L');
+  }  
+  startEndCheck(day) {
+    if (!day) {
+      return false;
+    }
+    if(this.startDate||this.endDate) {
+      return moment(this.startDate,'MM/DD/YYYY').isSame(day) || moment(this.endDate,'MM/DD/YYYY').isSame(day);
+    }  
+  }  
+
+  public isSelected(day){
+    if (!day) {
+    return false;
+    }
+    if(this.startDate&&this.endDate) {
+      return moment(day,'MM/DD/YYYY').isBetween(this.startDate,this.endDate);      
+    }    
+  }
+
+  public nextMonth() {
+    this.date.add(1, 'M');
+    this.daysArr = this.createCalendar(this.date);
+  }
+  public previousMonth() {
+    this.date.subtract(1, 'M');
+    this.daysArr = this.createCalendar(this.date);
+  }
+
+  public selectDate(day){
+    let dayFormatted = day.format('MM/DD/YYYY');
+    console.log(dayFormatted);
+    
+    if(this.startDate && this.endDate){
+      this.startDate = this.endDate = null;  
+      return  
+    }
+    if(!this.startDate){
+      this.startDate = dayFormatted;
+    } else {
+      this.endDate = dayFormatted;
+    }
+    
+  }
+
+
+
 
 }
